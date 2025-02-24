6e54328964fd4cdcdf4c8bb8055d8210d3634b05
alexandrepeclat <web.alexandre.peclat@gmail.com>
2025-02-24 22:06:24 +0100

    marche pas mal mais problèmes d'a coups

---
+++ nodeTestServer/index.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server running\n');
+});
+
+// Crée une instance WebSocket
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+
+const commands = [
+    'L010I1000', 
+    'L020I1000', 
+    'L030I1000', 
+    'L040I1000', 
+    'L050I1000', 
+    'L040I1000', 
+    'L030I1000', 
+    'L020I1000',
+    'L010I1000',
+    'L000I1000'
+];
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    
+    let count = 0;
+    const intervalId = setInterval(() => {
+        if (count < commands.length && ws.readyState === WebSocket.OPEN) {
+            // Crée un Buffer à partir de la commande
+            const commandBuffer = Buffer.from(commands[count], 'utf-8');
+            // Envoie le Buffer comme Blob
+            console.log("Sending ", commands[count]);
+            ws.send(commandBuffer);
+            count++;
+        } else {
+            clearInterval(intervalId);
+            ws.close(); // Ferme la connexion après l'envoi des 10 messages
+        }
+    }, 5000); // Envoie un message toutes les secondes
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Démarre le serveur sur le port 1234
+server.listen(1234, () => {
+    console.log('Server is listening on ws://localhost:1234');
+    const ipAddress = getLocalIPAddress();
+    console.log(`Server running at http://${ipAddress}:1234/`);
+});
+
+// Fonction pour obtenir l'adresse IP par défaut
+function getLocalIPAddress() {
+    const interfaces = os.networkInterfaces();
+    for (let iface in interfaces) {
+        for (let alias of interfaces[iface]) {
+            if (alias.family === 'IPv4' && !alias.internal) {
+                return alias.address; // On retourne la première adresse IPv4 non interne
+            }
+        }
+    }
+    return 'IP non trouvée';
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,109 +2,133 @@
 //  * ESP32 position motion control example with magnetic sensor
 //  */
 #include <Arduino.h>
-//  #include <ESPAsyncWebServer.h>
 #include <SimpleFOC.h>
+#include <WebSocketsClient.h>
 #include <WiFi.h>
 #include <secrets.h>
-#define PWM_PIN 14  // GPIO36 (ADC1_CH0) pour lire le signal PWM
+#define PWM_PIN 14
 
 // const int VELOCITY_LIMIT = 40;
 
 MagneticSensorPWM sensor = MagneticSensorPWM(PWM_PIN, 4, 904);
 
 // Motor instance
-    BLDCMotor motor = BLDCMotor(11);
+BLDCMotor motor = BLDCMotor(11);
 BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27, 15);
+
+// WebSocket client instance
+WebSocketsClient ws;
 
 // angle set point variable
 float target_angle = 0;
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_angle, cmd); }
+void doTarget(char* cmd) {
+  command.scalar(&target_angle, cmd);
+}
 
-// // Serveur WebSocket
-// // AsyncWebServer server(80);
-// // AsyncWebSocket ws("/ws");
+// Fonction pour analyser la commande reçue
+struct Command {
+  char action;
+  int axis;
+  int value;
+  int interval;
+  bool valid;
+};
 
-// // Fonction pour analyser la commande reçue
-// // struct Command {
-// //   char action;
-// //   int axis;
-// //   int value;
-// //   int interval;
-// //   bool valid;
-// // };
+Command parseCommand(const String& input) {
+  // Expression régulière pour extraire les données
+  // Format attendu : [Action][Axis][Value][Type][Interval]
+  if (input.length() >= 7) {  // La commande doit être d'une longueur suffisante
+    Command cmd;
+    cmd.action = input.charAt(0);               // 'L'
+    cmd.axis = input.charAt(1) - '0';           // '0' -> 0
+    cmd.value = input.substring(2, 4).toInt();  // '50' -> 50
+    cmd.interval = input.substring(5).toInt();  // '5000' -> 5000
+    cmd.valid = true;
+    return cmd;
+  } else {
+    Command cmd = {'\0', -1, -1, -1, false};  // Valeurs par défaut pour une commande invalide
+    return cmd;
+  }
+}
 
-// // Command parseCommand(const String& input) {
-// //   // Expression régulière pour extraire les données
-// //   // Format attendu : [Action][Axis][Value][Type][Interval]
-// //   if (input.length() >= 7) {                    // La commande doit être d'une longueur suffisante
-// //     Command cmd;
-// //     cmd.action = input.charAt(0);               // 'L'
-// //     cmd.axis = input.charAt(1) - '0';           // '0' -> 0
-// //     cmd.value = input.substring(2, 4).toInt();  // '50' -> 50
-// //     cmd.interval = input.substring(5).toInt();  // '5000' -> 5000
-// //     cmd.valid = true;
-// //     return cmd;
-// //   } else {
-// //     Command cmd = {'\0', -1, -1, -1, false};  // Valeurs par défaut pour une commande invalide
-// //     return cmd;
-// //   }
-// // }
+const float ANGLE_MIN = 0;
+const float ANGLE_MAX = 2 * PI;
+const int CMD_MIN = 0;
+const int CMD_MAX = 100;
 
-// // Fonction pour gérer les messages reçus via WebSocket
-// // void onWsEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len) {
-// //   if (type == WS_EVT_DATA) {
-// //     // Convertir les données en chaîne de caractères
-// //     String input = String((char*)data);
-// //     Serial.println("Command received: " + input);
+float mapFloat(float value, float inMin, float inMax, float outMin, float outMax) {
+  return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
 
-// //     // Parser la commande
-// //     Command cmd = parseCommand(input);
-// //     if (cmd.action == 'L') {
-// //       // Angle cible
-// //       float target_position = cmd.value;  // C'est la valeur que tu veux atteindre
-// //       // Angle actuel
-// //       float current_position = sensor.getAngle(); // Valeur actuelle de l'angle
-// //       // Calcul de la différence d'angle
-// //       float delta_angle = target_position - current_position;
-// //       // Calcul de la vitesse nécessaire (en degrés par seconde)
-// //       float velocity = delta_angle / (cmd.interval / 1000.0); // intervalle en secondes
+void executeCommand(Command cmd) {
+  if (cmd.action == 'L') {
+    Serial.println("CMD: A: " + String(cmd.axis) + " V: " + String(cmd.value) + " I: " + String(cmd.interval));
+    float target_position = mapFloat(cmd.value, CMD_MIN, CMD_MAX, ANGLE_MIN, ANGLE_MAX);
+    float current_position = sensor.getAngle();
+    float delta_angle = target_position - current_position;
+    float velocity = abs(delta_angle / (cmd.interval / 1000.0));
+    Serial.println("T: " + String(target_position) + " C: " + String(current_position) + " D: " + String(delta_angle) + " V: " + String(velocity));
 
-// //       // S'assurer que la vitesse ne dépasse pas la limite de vitesse maximale
-// //       if (velocity > VELOCITY_LIMIT) {
-// //         velocity = VELOCITY_LIMIT;
-// //       }
+    // S'assurer que la vitesse ne dépasse pas la limite de vitesse maximale
+    if (velocity > 20) {
+      velocity = 20;
+    }
 
-// //       // Définir la vitesse et l'angle cible
-// //       target_angle = target_position;
-// //       motor.velocity_limit = velocity;  // Utilise la vitesse calculée
-// //     }
-// //   } else if (type == WS_EVT_CONNECT) {
-// //     Serial.println("WebSocket client connected");
-// //   } else if (type == WS_EVT_DISCONNECT) {
-// //     Serial.println("WebSocket client disconnected");
-// //   }
-// // }
+    // Définir la vitesse et l'angle cible
+    target_angle = target_position;
+    //motor.velocity_limit = velocity;
+  }
+}
+
+// Fonction pour gérer les messages reçus via WebSocket
+void onWsEvent(WStype_t type, uint8_t* payload, size_t length) {
+  Serial.println();
+  if (type == WStype_TEXT) {
+    String input = String((char*)payload);
+    Serial.println("Command received: " + input);
+    Command cmd = parseCommand(input);
+    executeCommand(cmd);
+  } else if (type == WStype_BIN) {
+    // Traitement des données binaires (Blob)
+    Serial.print("Received Binary Data:");
+    for (size_t i = 0; i < length; i++) {
+      Serial.print(payload[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+    String input = String((char*)payload);
+    Serial.println("Command received: " + input);
+    Command cmd = parseCommand(input);
+    executeCommand(cmd);
+  } else if (type == WStype_CONNECTED) {
+    Serial.println("WebSocket connected");
+    ws.sendTXT("Hello Server");  // Envoie un message initial
+  } else if (type == WStype_DISCONNECTED) {
+    Serial.println("WebSocket disconnected");
+  } else if (type == WStype_ERROR) {
+    Serial.println("WebSocket error");
+  }
+}
 
 void doPWM() {
   sensor.handlePWM();
 }
 
-
 void setup() {
-
   Serial.begin(115200);
-  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(1000);
-  //   Serial.println("Connecting to WiFi...");
-  // }
-  // Serial.println("Connected to WiFi!");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi!");
 
   // Initialisation du serveur WebSocket
-  // ws.onEvent(onWsEvent);
-  // server.addHandler(&ws);
+  ws.begin("192.168.0.173", 1234, "/");
+  ws.onEvent(onWsEvent);
+  ws.setReconnectInterval(5000);
 
   // enable more verbose output for debugging
   // comment out if not needed
@@ -147,10 +171,9 @@ void setup() {
   motor.P_angle.P = 20;
   // maximal velocity of the position control
   motor.velocity_limit = 20;
-  
+
   // comment out if not needed
   motor.useMonitoring(Serial);
-
 
   // initialize motor
   motor.init();
@@ -165,9 +188,8 @@ void setup() {
   _delay(1000);
 }
 
-
 void loop() {
-
+  ws.loop();
   // main FOC algorithm function
   // the faster you run this function the better
   // Arduino UNO loop  ~1kHz
@@ -180,7 +202,6 @@ void loop() {
   // You can also use motor.move() and set the motor.target in the code
   motor.move(target_angle);
 
-
   // function intended to be used with serial plotter to monitor motor variables
   // significantly slowing the execution down!!!!
   // motor.monitor();
@@ -188,4 +209,3 @@ void loop() {
   // user communication
   command.run();
 }
-
