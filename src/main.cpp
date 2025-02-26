ee4e32e844abfc728c6e0553f0e703bfdea5a50a
alexandrepeclat <web.alexandre.peclat@gmail.com>
2025-02-26 22:19:40 +0100

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,9 @@
 #include <WebSocketsClient.h>
 #include <WiFi.h>
 #include <secrets.h>
-
+#include <limits>
 enum AppState {
-  CALIBRATING_MIN,
-  CALIBRATING_MAX,
+  CALIBRATING_MANUALLY,
   RUNNING,
   STOPPED
 };
@@ -30,8 +29,8 @@ BLDCDriver3PWM driver = BLDCDriver3PWM(25, 26, 27, 15);
 WebSocketsClient ws;
 
 // Variables pour stocker les limites min et max
-float minAngle = 0;
-float maxAngle = 0;
+float minAngle = std::numeric_limits<float>::max();
+float maxAngle = std::numeric_limits<float>::min();
 
 // angle set point variable
 float target_angle = 0;
@@ -46,20 +45,20 @@ void doStop(char* cmd) {
   appState = AppState::STOPPED;
 }
 
+void doRun(char* cmd) {
+  appState = AppState::RUNNING;
+}
+
 void doCalibration(char* cmd) {
-  appState = AppState::CALIBRATING_MIN;
-  target_angle = sensor.getAngle() - (4 * PI);  // Déplacement vers la butée min
+  appState = AppState::CALIBRATING_MANUALLY;
+  motor.disable();                               // Désactiver le moteur pour calibration manuelle
+  minAngle = std::numeric_limits<float>::max();  // Valeur initiale très haute
+  maxAngle = std::numeric_limits<float>::min();  // Valeur initiale très basse
   Serial.println("Calibration started " + String(motor.target));
 }
 
-void doSetMinAngle(char* cmd) {
-  minAngle = sensor.getAngle();
-  Serial.println("Min angle set to: " + String(minAngle));
-}
-
-void doSetMaxAngle(char* cmd) {
-  maxAngle = sensor.getAngle();
-  Serial.println("Max angle set to: " + String(maxAngle));
+bool isCalibrated() {
+  return minAngle != std::numeric_limits<float>::max() && maxAngle != std::numeric_limits<float>::min();
 }
 
 void doPrintDebug(char* cmd) {
@@ -100,6 +99,14 @@ float mapFloat(float value, float inMin, float inMax, float outMin, float outMax
 }
 
 void executeCommand(Command cmd) {
+  if (!isCalibrated()) {
+    Serial.println("Command ignored: Motor not calibrated yet");
+    return;
+  }
+  if (appState != AppState::RUNNING) {
+    Serial.println("Command ignored: Motor not running");
+    return;
+  }
   if (cmd.action == 'L') {
     Serial.println("CMD: A: " + String(cmd.axis) + " V: " + String(cmd.value) + " I: " + String(cmd.interval));
     float target_position = mapFloat(cmd.value, CMD_MIN, CMD_MAX, minAngle, maxAngle);
@@ -107,18 +114,17 @@ void executeCommand(Command cmd) {
     float current_shaft_position = motor.shaft_angle;  // semble identique à sensor.getAngle()
     float delta_angle = target_position - current_position;
     float velocity = abs(delta_angle / (cmd.interval / 1000.0));
-    Serial.println("T: " + String(target_position) + " C(s): " + String(current_position) + " C(m): " + current_shaft_position + " D: " + String(delta_angle) + " V: " + String(velocity));
-    Serial.print(target_position, 10);
+    float target_angle_rounded = round(target_position * 100) / 100.0;
+
+    Serial.println("T: " + String(target_angle_rounded) + " C(s): " + String(current_position) + " C(m): " + current_shaft_position + " D: " + String(delta_angle) + " V: " + String(velocity));
     // S'assurer que la vitesse ne dépasse pas la limite de vitesse maximale
     if (velocity > 20) {
       velocity = 20;
     }
 
     // Définir la vitesse et l'angle cible
-    float roundedValue = round(target_position * 100) / 100.0;
-    Serial.println("Rounded value: " + String(roundedValue));
     noInterrupts();
-    target_angle = roundedValue;
+    target_angle = target_angle_rounded;
     interrupts();
     motor.velocity_limit = velocity;
   }
@@ -222,12 +228,11 @@ void setup() {
   motor.initFOC();
 
   // add target command T
-  command.add('T', doTarget, "target angle");
-  command.add('S', doStop, "stop");
-  command.add('C', doCalibration, "calibrate");
-  command.add('M', doSetMinAngle, "set min angle");
-  command.add('X', doSetMaxAngle, "set max angle");
-  command.add('D', doPrintDebug, "print debug");
+  command.add('t', doTarget, "target angle");
+  command.add('s', doStop, "stop");
+  command.add('r', doRun, "run");
+  command.add('c', doCalibration, "calibrate");
+  command.add('d', doPrintDebug, "print debug");
 
   Serial.println(F("Motor ready."));
   Serial.println(F("Set the target angle using serial terminal:"));
@@ -236,89 +241,51 @@ void setup() {
 
 // Fonction pour calibrer les limites du moteur
 void calibrateLimits() {
-  static float lastAngle = sensor.getAngle();
-  static unsigned long lastTime = millis();         // Temps du dernier calcul de vitesse
-  static unsigned long stallStartTime = 0;          // Temps depuis le dernier "stall" détecté
-  const float velocityThreshold = 0.1;              // Seuil de vitesse pour détecter un mouvement insuffisant
-  const unsigned long velocityCheckInterval = 200;  // Vérifier la vitesse toutes les 200 ms
-
-  unsigned long currentTime = millis();
-  float currentAngle = sensor.getAngle();
-
-  // Si l'intervalle de temps est dépassé, on calcule la vitesse
-  if (currentTime - lastTime >= velocityCheckInterval) {
-    // Calcul de la vitesse en fonction de l'angle
-    float deltaAngle = fabs(currentAngle - lastAngle);
-    float deltaTime = (currentTime - lastTime) / 1000.0;  // Temps écoulé en secondes
-    float velocity = deltaAngle / deltaTime;              // Vitesse en degrés par seconde
-    Serial.println("Velocity: " + String(velocity) + " DeltaTime: " + String(deltaTime) + " DeltaAngle: " + String(deltaAngle));
-
-    // Si la vitesse est inférieure au seuil, on détecte un "stall"
-    if (velocity < velocityThreshold) {
-      if (stallStartTime == 0) {
-        stallStartTime = currentTime;  // On commence à compter le temps du "stall"
-      }
-      // Si le moteur reste bloqué pendant l'intervalle de vérification, on détecte le "stall"
-      if (currentTime - stallStartTime >= velocityCheckInterval) {
-        if (appState == CALIBRATING_MIN) {
-          minAngle = currentAngle;  // Enregistrer la position minimale
-          appState = CALIBRATING_MAX;
-          Serial.println("Min angle detected: " + String(minAngle));
-          target_angle = currentAngle + 4 * PI;  // Déplacer vers l'autre extrémité pour le calibrage max
-          stallStartTime = 0;                    // Réinitialiser le temps de blocage
-        } else if (appState == CALIBRATING_MAX) {
-          maxAngle = currentAngle;  // Enregistrer la position maximale
-          appState = STOPPED;       // Terminer la calibration et mettre en STOPPED
-          Serial.println("Max angle detected: " + String(maxAngle));
-          target_angle = currentAngle;  // Arrêter le moteur à la position actuelle
-          stallStartTime = 0;           // Réinitialiser le temps de blocage
-        }
-      }
-    } else {
-      stallStartTime = 0;  // Réinitialiser si le moteur bouge (vitesse suffisante)
-    }
-
-    lastTime = currentTime;  // Mettre à jour le temps du dernier calcul de vitesse
-  }
-
-  lastAngle = currentAngle;  // Mettre à jour l'angle précédent
 }
 
 void loop() {
-   ws.loop();
+  ws.loop();
   switch (appState) {
-    case CALIBRATING_MIN:
-    case CALIBRATING_MAX:
-    if (!motor.enabled) {
-      motor.enable();
-    }
-      motor.voltage_limit = 3;     // Réduire la tension pour éviter une force excessive
-      motor.velocity_limit = 0.5;  // Vitesse lente pour la calibration
-      motor.loopFOC();
-      motor.move(target_angle);
-      // Serial.println("Moving to target: " + String(target_angle) + " Current angle: " + String(sensor.getAngle()));
-      calibrateLimits();  // Vérifier si le moteur est bloqué
-      break;
-    case RUNNING:
-    if (!motor.enabled) {
-      motor.enable();
-    }
-      motor.voltage_limit = 6;    // maximal voltage to be set to the motor
-      motor.velocity_limit = 20;  // maximal velocity of the position control
-      motor.loopFOC();            // main FOC algorithm function the faster you run this function the better
-      motor.move(target_angle);   // Motion control function velocity, position or voltage (defined in motor.controller) this function can be run at much lower frequency than loopFOC() function
-      break;
-    case STOPPED:
+    case CALIBRATING_MANUALLY: {
       if (motor.enabled) {
         motor.disable();
       }
-      target_angle = sensor.getAngle();
+      float currentAngle = sensor.getAngle();
+      target_angle = currentAngle; // Set target angle as the current angle, for not "jumping" when starting
+      if (currentAngle < minAngle) {
+        minAngle = currentAngle;
+        Serial.println("Min-max angles: " + String(minAngle) + " " + String(maxAngle));
+      }
+      if (currentAngle > maxAngle) {
+        maxAngle = currentAngle;
+        Serial.println("Min-max angles: " + String(minAngle) + " " + String(maxAngle));
+      }
+      break;
+    }
+    case RUNNING: {
+      if (!motor.enabled) {
+        motor.enable();
+      }
+      motor.voltage_limit = 6;    // maximal voltage to be set to the motor
+      motor.velocity_limit = 20;  // maximal velocity of the position control
+      break;
+    }
+    case STOPPED: {
+      if (motor.enabled) {
+        motor.disable();
+      }
+      float currentAngle = sensor.getAngle();
+      target_angle = currentAngle; // Set target angle as the current angle, for not "jumping" when starting
       motor.voltage_limit = 0;
       motor.velocity_limit = 0;
-      motor.loopFOC();     // Mainly for updating sensors
-      motor.move(target_angle);
       break;
+    }
   }
+
+  // main FOC algorithm function the faster you run this function the better //TODO à voir si loopFoc doit être fait avant les trucs dans le switch
+  // Even if motor is stopped, For updating sensors
+  motor.loopFOC();
+  motor.move(target_angle);  // Motion control function velocity, position or voltage (defined in motor.controller) this function can be run at much lower frequency than loopFOC() function
 
   // user communication
   command.run();
